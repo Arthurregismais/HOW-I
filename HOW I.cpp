c3726adc84e269aced3dcae996253ec26c2931fe
Arthur <arthur.regismais@hotmail.com>
2022-03-28 16:10:05 -0300

    Adicionar arquivos de projeto.

---
+++ HOW I.cpp
@@ -0,0 +1,19 @@
+// HOW I.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Calculadora Decimal!\n";
+    std::cout << "1 - Decimal para binario\n";
+    std::cout << "2 - Binario para decimal\n";
+    std::cout << "Digite um valor:\n";
+    std::cin >> a;
+    std::cout << "O valor digitado foi " << a << "\n";
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
